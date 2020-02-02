4022fd96b960f57d4380807ebdb9f62fc259ba12
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-02-03 00:33:16 +0300

    implement audio player app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +10,34 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              button(Colors.amber, 1),
+              button(Colors.indigo, 2),
+              button(Colors.blueAccent, 3),
+              button(Colors.cyanAccent, 4),
+              button(Colors.green, 7),
+              button(Colors.red, 5),
+              button(Colors.teal, 6),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget button(Color col, int soundNum) {
+  return Expanded(
+      child: FlatButton(
+          onPressed: () {
+            playsound(soundNum);
+          },
+          color: col));
+}
+
+void playsound(int soundNum) {
+  final audio = AudioCache();
+  audio.play('note$soundNum.wav');
 }
